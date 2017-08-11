972be4977a4a2f32759ca5cde0ea10ce500680a5
TYQbag <1104295080@qq.com>
2017-08-11 17:28:20 +0800

    补上缺掉的内容

---
+++ Love520/Category/UIButton+Layout.h
@@ -0,0 +1,16 @@
+//
+//  UIButton+Layout.h
+//  YLButton
+//
+//  Created by HelloYeah on 2016/12/5.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (Layout)
+
+@property (nonatomic,assign) CGRect titleRect;
+@property (nonatomic,assign) CGRect imageRect;
+
+@end
